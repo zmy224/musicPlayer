5e86b4d3c5064d79e58c6da7d5a7a7f0f728ac39
zmy <2391720762@qq.com>
2020-10-31 22:35:27 +0800

    首页tab提交

---
+++ project/src/components/home/header.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- header组件 -->
+    <div class="header">
+      <img src="../../assets/header.png" alt=""> <span class="headetitle">Chicken Music</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'headerNav'
+}
+</script>
+
+<style>
+.header  {
+    height:44px;
+    width:100%;
+    margin: 0 auto;
+  
+}
+  .header img  {
+      height: 100%;
+     display: inline-block;
+
+    }
+    .header .headetitle {
+        color:gold;
+        font-size:20px;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
+
+    }
+</style>
+
+
---
+++ project/src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home"> 
+   <!-- 顶部 -->
+   <header-nav></header-nav>
+   <!-- tab -->
+   <ul class="tab">
+     <li @click='tabTaggle("recommend")'>推荐</li>
+     <li @click='tabTaggle("hh")'>人气</li>
+     <li @click='tabTaggle("singer")'>歌手</li>
+     <li @click='tabTaggle("music")'>音乐</li>
+   </ul>
+   <!-- 动态组件 -->
+   <component :is="currentTab"></component>
+  </div>
+</template>
+
+<script>
+import headerNav from '../components/home/header.vue'
+import recommend from  './recommend.vue'
+import singer from  './singer.vue'
+export default {
+ components:{
+   headerNav,
+   recommend,
+   singer
+ },
+ data(){
+   return  {
+     currentTab:'recommend'
+   }
+ },
+ methods: {
+   tabTaggle(taggleMenu){
+     this.currentTab=taggleMenu;
+   }
+ },
+}
+</script>
+
+<style>
+.home{
+  background-color: #272727;
+  margin: 0;
+  height: 100%;
+  width:100%;
+}
+.home ul {
+  list-style: none;
+  display: flex;
+ margin:10px 0;
+ padding:0;
+
+}
+.home ul li  {
+   list-style: none;
+   flex:1;
+   color:rgb(138, 135, 135);
+}
+.home ul li:active{
+  color:gold;
+}
+</style>
