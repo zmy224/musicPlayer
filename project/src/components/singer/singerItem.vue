b1d94fa82784f2e32c0228646a07891582f825af
zmy <2391720762@qq.com>
2020-11-23 22:49:24 +0800

    歌手tab代码提交

---
+++ project/src/components/singer/singerItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="heade">
+      <img :src="person.id" alt=""> <span>person.name</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        person:{
+            default:{},
+            type:Object
+        }
+    }
+}
+</script>
+
+<style scoped>
+.heade{
+    width:100%;
+    height:48px;
+}
+</style>
--- project/src/views/singer.vue
+++ project/src/views/singer.vue
@@ -1,12 +1,68 @@
 <template>
     <div>
         我是歌手tab
+        
     </div>
 </template>
 
 <script>
+import  { getSingerOrigin } from '../api/singer.js'
+import personHeader from '../components/singer/singerItem.vue'
 export default {
-    
+    components:{
+personHeader
+    },
+    data(){
+        return  {
+               singerList:{},
+
+        }
+    },
+    mounted() {
+        this.getSingerList();
+    },
+    methods: {
+        getSingerList(){
+            getSingerOrigin().then(res=>{
+                console.log(res,'歌手列表');
+                this.convenceData(res.data.list)
+
+            })
+        },
+        convenceData(list){
+        let mapRelation= {
+            hot:{}
+        };
+        mapRelation.hot.items =[];
+        mapRelation.hot.title='热门';
+        for(let i=0;i<list.length;i++){
+          if(i<10){
+            let obj = {
+            id:list[i].Fsinger_id,
+            name:list[i].Fsinger_name
+           }
+          mapRelation.hot.items.push(obj);
+       }
+         let findex=list[i].Findex;  
+         mapRelation[findex]={
+             title:'',
+             items:[]
+         }
+        }    
+     for(let key in mapRelation){
+         mapRelation[key].title=key;
+         mapRelation[key].items=list.filter(item=>item.Findex==key).map(mapitem=>{
+          return  {
+                 id:mapitem.Fsinger_id,
+                 name:mapitem.Fsinger_name
+             }
+         })
+            
+     
+     }
+        console.log(mapRelation,'mapRelation');
+        }
+    },
 }
 </script>
 
